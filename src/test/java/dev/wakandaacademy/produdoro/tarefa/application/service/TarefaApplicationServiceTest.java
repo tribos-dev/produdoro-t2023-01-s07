952f5d56d21d49c5961269a985d75350db680f66
Ronan Lima <ronanlimasilva@hotmail.com>
2023-07-27 21:36:57 -0300

    implementa teste unitario

--- src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
@@ -4,50 +4,68 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaDetalhadoResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaIdResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
 import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
 
 @ExtendWith(MockitoExtension.class)
 class TarefaApplicationServiceTest {
 
-    //	@Autowired
-    @InjectMocks
-    TarefaApplicationService tarefaApplicationService;
+	// @Autowired
+	@InjectMocks
+	TarefaApplicationService tarefaApplicationService;
 
-    //	@MockBean
-    @Mock
-    TarefaRepository tarefaRepository;
+	// @MockBean
+	@Mock
+	TarefaRepository tarefaRepository;
+	@Mock
+	UsuarioRepository usuarioRepository;
 
-    @Test
-    void deveRetornarIdTarefaNovaCriada() {
-        TarefaRequest request = getTarefaRequest();
-        when(tarefaRepository.salva(any())).thenReturn(new Tarefa(request));
+	@Test
+	void deveRetornarIdTarefaNovaCriada() {
+		TarefaRequest request = getTarefaRequest();
+		when(tarefaRepository.salva(any())).thenReturn(new Tarefa(request));
 
-        TarefaIdResponse response = tarefaApplicationService.criaNovaTarefa(request);
+		TarefaIdResponse response = tarefaApplicationService.criaNovaTarefa(request);
 
-        assertNotNull(response);
-        assertEquals(TarefaIdResponse.class, response.getClass());
-        assertEquals(UUID.class, response.getIdTarefa().getClass());
-    }
+		assertNotNull(response);
+		assertEquals(TarefaIdResponse.class, response.getClass());
+		assertEquals(UUID.class, response.getIdTarefa().getClass());
+	}
 
+	public TarefaRequest getTarefaRequest() {
+		TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
+		return request;
+	}
 
+	@Test
+	@DisplayName("Deve retornar uma lista com todas as tarefas")
+	void deveListarTodasTarefasDoUsuario() {
+		Usuario usuario = DataHelper.createUsuario();
+		UUID usuario1 = usuario.getIdUsuario();
+		when(usuarioRepository.buscaUsuarioPorEmail(anyString())).thenReturn(usuario);
+		when(tarefaRepository.buscarTodasTarefas(any(UUID.class))).thenReturn(DataHelper.createListTarefa());
+		List<TarefaDetalhadoResponse> lista = tarefaApplicationService.listaTodasTarefas("email@email.com", usuario1);
+		assertNotNull(lista);
+		assertFalse(lista.isEmpty());
+	}
 
-    public TarefaRequest getTarefaRequest() {
-        TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
-        return request;
-    }
 }
